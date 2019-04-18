66e01850cef9d443df14c954efcd4460ad16228b
mridul-ganga <mridul-ganga@hpe.com>
2019-04-18 09:00:51 +0530

    navbar config

--- assets/main.js
+++ assets/main.js
@@ -2,16 +2,18 @@
 
 $.get('nav.html',function(response){ 
     $('#nav').html(response); 
+
+
+    // If user is logged in
+    if (localStorage.getItem("username")){
+        var username = localStorage.getItem("username");
+        console.log(username);
+        $("username_view").html(username);
+
+        $("#logout").click(function(){
+            localStorage.removeItem("username");
+            window.location.href = "login.html";
+        });
+    }
+
 });
-
-
-// If user is logged in
-if (localStorage.getItem("username")){
-    var username = localStorage.getItem("username");
-    $("username_view").html(username);
-
-    $("#logout").click(function(){
-        localStorage.removeItem("username");
-        window.location.href = "login.html";
-    });
-}
